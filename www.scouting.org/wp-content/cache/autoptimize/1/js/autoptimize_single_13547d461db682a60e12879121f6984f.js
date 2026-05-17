3e03dcae1beb2f8259b04a030781ecb9e2ab2740
root <root@vmi3271488.contaboserver.net>
2026-05-17 10:00:44 +0200

    Surgical Clone: Scouting.org refactored for Vercel

---
+++ www.scouting.org/wp-content/cache/autoptimize/1/js/autoptimize_single_13547d461db682a60e12879121f6984f.js
@@ -0,0 +1,2 @@
+function isZipCode(value){return/(^\d{5}$)/.test(value);}
+jQuery(function($){$(".scouting_beascout_form").on("submit",function(e){var zipcode=$(this).find('.zipcode');if(!isZipCode(zipcode.val())){e.preventDefault();alert("Please enter a five digit zipcode");}});});
